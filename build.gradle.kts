6733f77e674a590ee8054926a3dbb62dc08bbb63
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-03-18 03:47:25 -0300

    Mejora Build Gradle Raiz

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    // Aquí declaramos las versiones de los plugins, pero NO los aplicamos a la raíz (apply false)
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("org.jetbrains.kotlin.jvm") version "1.9.22" apply false
