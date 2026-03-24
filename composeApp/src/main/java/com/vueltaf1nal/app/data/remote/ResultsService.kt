ea2521fd2dce021837bfa93526fe3150e2580742
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-03-24 12:12:55 -0300

    Armo Services DriverResult y MaxSpeed

---
+++ composeApp/src/main/java/com/vueltaf1nal/app/data/remote/ResultsService.kt
@@ -0,0 +1,11 @@
+package com.vueltaf1nal.app.data.remote
+
+import com.vueltaf1nal.app.data.models.api.DriverResultDto
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+
+class ResultsService {
+    suspend fun getResults(): List<DriverResultDto> {
+        return NetworkClient.client.get("${NetworkClient.BASE_URL}/api/results").body()
+    }
+}
---
+++ composeApp/src/main/java/com/vueltaf1nal/app/data/remote/TopSpeedsService.kt
@@ -0,0 +1,11 @@
+package com.vueltaf1nal.app.data.remote
+
+import com.vueltaf1nal.app.data.models.api.MaxSpeedDto
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+
+class TopSpeedsService {
+    suspend fun getTopSpeeds(): List<MaxSpeedDto> {
+        return NetworkClient.client.get("${NetworkClient.BASE_URL}/api/max-speed").body()
+    }
+}
